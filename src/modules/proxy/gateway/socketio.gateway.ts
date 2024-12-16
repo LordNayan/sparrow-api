bf64dcc192256be342e5136ed2b8accf96de6ad2
Md Asif Raza <itsmdasifraza@gmail.com>
2024-12-16 22:14:44 +0530

    feat: implement socketIO proxy cloud agent [#2183]

--- src/modules/proxy/gateway/socketio.gateway.ts
+++ src/modules/proxy/gateway/socketio.gateway.ts
@@ -2,26 +2,23 @@ import {
   WebSocketGateway,
   WebSocketServer,
   OnGatewayConnection,
-  OnGatewayDisconnect,
 } from "@nestjs/websockets";
-import { Server, Socket } from "socket.io";
+import { Server, Socket as SocketServer } from "socket.io";
 import { SocketIoService } from "../services/socketio.service";
 
 export const SOCKET_IO_PORT = 9001;
 
 @WebSocketGateway(SOCKET_IO_PORT, {
   cors: {
-    origin: "*", // Replace "*" with your frontend URL in production
+    origin: "*",
     methods: ["GET", "POST"],
   },
   path: "/socket.io",
   transports: ["websocket"],
 })
-export class SocketIoGateway implements OnGatewayConnection, OnGatewayDisconnect {
+export class SocketIoGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
-
-  private clientSockets = new Map<Socket, any>();
 
   constructor(private readonly socketIoService: SocketIoService) {}
 
@@ -29,11 +26,11 @@ export class SocketIoGateway implements OnGatewayConnection, OnGatewayDisconnect
     console.log("Socket.io Handler Gateway initialized!");
   }
 
-  async handleConnection(client: Socket) {
-    const { targetUrl, namespace, headers } = client.handshake.query;
+  async handleConnection(proxySocketIO: SocketServer) {
+    const { targetUrl, namespace, headers } = proxySocketIO.handshake.query;
 
     if (!targetUrl || !namespace) {
-      client.disconnect();
+      proxySocketIO.disconnect();
       console.error(
         "Missing required query parameters: url, namespace, or tabid",
       );
@@ -41,42 +38,49 @@ export class SocketIoGateway implements OnGatewayConnection, OnGatewayDisconnect
     }
 
     try {
-      const parsedHeaders = headers ? JSON.parse(headers as string) : {};
-
-      // Establish a connection to the real Socket.IO server
-      const targetSocketIO = await this.socketIoService.connectToRealSocket(
-        client,
-        targetUrl as string,
-        namespace as string,
-        parsedHeaders,
+      const parsedHeaders = JSON.parse(headers as string);
+      const headersObject: { [key: string]: string } = parsedHeaders.reduce(
+        (
+          acc: Record<string, string>,
+          { key, value }: { key: string; value: string },
+        ) => {
+          acc[key] = value;
+          return acc;
+        },
+        {} as { [key: string]: string },
       );
 
-      // Store the target socket for this client
-      this.clientSockets.set(client, targetSocketIO);
+      // Establish a connection to the real Socket.IO server
+      const targetSocketIO = await this.socketIoService.connectToTargetSocketIO(
+        proxySocketIO,
+        targetUrl as string,
+        namespace as string,
+        headersObject,
+      );
 
-      // Listen for all dynamic events from the frontend and forward them
-      client.onAny(async (event: string, ...args: any[]) => {
+      proxySocketIO.on("disconnect", async () => {
+        // Disconnecting target Socket.IO will automatically disconnects proxy Socket.IO in chain.
+        targetSocketIO?.disconnect();
+      });
+
+      // Listen for all dynamic events from the frontend and forward them to target Socket.IO.
+      proxySocketIO.onAny(async (event: string, args: any) => {
         try {
-          targetSocketIO.emit(event, ...args);
+          if (event === "sparrow_internal_disconnect") {
+            // Disconnecting target Socket.IO will automatically disconnects proxy Socket.IO in chain.
+            targetSocketIO?.disconnect();
+          } else {
+            targetSocketIO?.emit(event, args);
+          }
         } catch (err) {
-          console.error(
-            `Failed to forward event ${event} for ${err.message}`,
-          );
+          console.error(`Failed to forward event ${event} for ${err.message}`);
         }
       });
     } catch (err) {
       console.error(
         `Failed to connect to real Socket.IO server for ${err.message}`,
       );
-      client.disconnect();
-    }
-  }
-
-  async handleDisconnect(client: Socket) {
-    const targetSocketIO = this.clientSockets.get(client);
-    if (targetSocketIO) {
-      targetSocketIO.disconnect();
-      this.clientSockets.delete(client);
+      proxySocketIO.disconnect();
     }
   }
 }
--- src/modules/proxy/services/socketio.service.ts
+++ src/modules/proxy/services/socketio.service.ts
@@ -1,47 +1,46 @@
 import { Injectable } from "@nestjs/common";
-import { io, Socket } from "socket.io-client";
+import { io, Socket as SocketClient } from "socket.io-client";
+import { Socket as SocketServer } from "socket.io";
 
 @Injectable()
 export class SocketIoService {
   /**
-   * Connect to the real Socket.IO server with a specific `tabid`.
+   * Makes a connection to the target Socket.IO server.
    */
-  async connectToRealSocket(
-    proxySocketIO: any,
-    targetUrl: string,
-    namespace: string,
-    headers: Record<string, string> = {},
-  ): Promise<any> {
-
-    const realSocket = io(`${targetUrl}${namespace}`, {
-      transports: ["websocket"],  
-      extraHeaders: headers,
+  async connectToTargetSocketIO(
+    _proxySocketIO: SocketServer,
+    _targetUrl: string,
+    _namespace: string,
+    _headers: Record<string, string> = {},
+  ): Promise<SocketClient> {
+    const _targetSocketIO = io(`${_targetUrl}${_namespace}`, {
+      transports: ["websocket"],
+      extraHeaders: _headers,
+      reconnection: false,
     });
 
-      realSocket.on("connect_error", (err) => {
-        proxySocketIO.disconnect();
-      });
-    
-       realSocket.on("disconnect", (err) => {
-        proxySocketIO.disconnect();
-      });
-    
-
-    // Listen for all dynamic events from the real server and forward to the frontend
-    realSocket.onAny((event: string, ...args: any[]) => {
-      console.log(proxySocketIO, "===================================");
-      console.log(event);
-      proxySocketIO.emit(event, args);
+    // Listen for connect events from the target Socket.IO and forward to the proxy Socket.IO.
+    _targetSocketIO.on("connect", () => {
+      _proxySocketIO?.emit("sparrow_internal_connect", "io server connect");
     });
 
-    // Handle disconnection of the real server
-    realSocket.on("disconnect", (reason) => {
-      console.warn(
-        `Real Socket.IO server disconnected for: ${reason}`,
-      );
-
+    // Listen for connect error events from the target Socket.IO and forward to the proxy Socket.IO.
+    _targetSocketIO.on("connect_error", (err) => {
+      _proxySocketIO?.emit("sparrow_internal_connect_error", err.message);
+      _proxySocketIO?.disconnect();
     });
-    return realSocket;
+
+    // Listen for disconnect events from the target Socket.IO and forward to the proxy Socket.IO.
+    _targetSocketIO.on("disconnect", (err) => {
+      _proxySocketIO?.emit("sparrow_internal_disconnect", err);
+      _proxySocketIO?.disconnect();
+    });
+
+    // Listen for all dynamic events from the target Socket.IO and forward to the proxy Socket.IO.
+    _targetSocketIO.onAny((event: string, ...args: any[]) => {
+      _proxySocketIO?.emit(event, args);
+    });
+
+    return _targetSocketIO;
   }
-
 }
